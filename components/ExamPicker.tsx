3dc3326f82c6870b7eba0987efe87723d3c5b25b
Szymon Sosnowski <szymon@stem-kolo.pl>
2026-06-02 07:43:08 +0200

    fix: spacja w naglowkach sesji, max-height panelu 2000px

--- components/ExamPicker.tsx
+++ components/ExamPicker.tsx
@@ -81,7 +81,7 @@ export function ExamPicker({
                 {sessions.map((session) => (
                     <div key={`${session.year}-${session.month}`} className="exam-session">
                         <h4 className="exam-session-header">
-                            {session.month.toUpperCase()}&nbsp;{session.year}
+                            {session.month.toUpperCase()} {session.year}
                         </h4>
 
                         {session.exams.length > 0 ? (
